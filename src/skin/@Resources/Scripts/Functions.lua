901685af47b3828d9314c110ac9fdf0eac6ce807
Sergienko Anton <sergienkoanton@mail.ru>
2019-01-09 21:41:27 +0700

    Add example of Lua script

---
+++ src/skin/@Resources/Scripts/Functions.lua
@@ -0,0 +1,13 @@
+function ConvertToHex(color) -- Converts RGB colors to HEX
+	local hex = {}
+
+	for rgb in color:gmatch('%d+') do
+		table.insert(hex, ('%02X'):format(tonumber(rgb)))
+	end
+
+	return table.concat(hex)
+end
+
+function ConvertPercentToHex(percent) -- Converts percent of opacity to HEX alpha
+	return ConvertToHex(tostring(255 * percent / 100))
+end
