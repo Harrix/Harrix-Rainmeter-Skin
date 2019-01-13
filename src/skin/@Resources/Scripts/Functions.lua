2a66ba72aa8ceb619ae7e6f37e763c4ae4283950
Sergienko Anton <sergienkoanton@mail.ru>
2019-01-13 21:45:11 +0700

    Move scripts in the folder Scripts

--- src/skin/@Resources/Scripts/Functions.lua
+++ src/skin/@Resources/Scripts/Functions.lua
@@ -1,3 +1,7 @@
 function ConvertPercentToHex(percent) -- Converts percent of opacity to HEX alpha
   return ('%02X'):format(math.floor(255 * percent / 100))
 end
+
+function ConvertPercentToDec(percent) -- Converts percent of opacity to DEC alpha
+  return math.floor(255 * percent / 100)
+end
